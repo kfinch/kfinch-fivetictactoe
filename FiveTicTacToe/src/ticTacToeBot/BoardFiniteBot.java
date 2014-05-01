8e8a66e8755e87d041125cf2452c38998019551a
Kelton Finch <keltonfinch@gmail.com>
2014-05-01 03:43:21 -0400

    Continued adding bot functionality to this project, moving relevant classes to a new 'ticTacToeBot' package.
    Added a toString method to Square

---
+++ FiveTicTacToe/src/ticTacToeBot/BoardFiniteBot.java
@@ -0,0 +1,221 @@
+package ticTacToeBot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import backEnd.BoardFinite;
+import backEnd.Square;
+import backEnd.Symbol;
+
+/**
+ * An extension of BoardFinite with additional methods for use by an AI player of the game.
+ * 
+ * This class includes a static evaluator function, and it's current implementation is janky as fuck.
+ * It works by looking at how many streak in a row each player has, and evaluating based on how long
+ * those streak are and how much open space they have around them. This could cause some weird evals,
+ * but it's the best I have for now. The evaluation function can easily be changed if I think of something better.
+ * 
+ * @author Kelton Finch
+ */
+public class BoardFiniteBot extends BoardFinite {
+
+	//I have to redeclare these for some reason t_t
+	public final int xDim;
+	public final int yDim;
+	
+	private boolean hasChecked[][];
+	
+	private static final int DIRECTIONS[] = {0,1,1,1,1,0,1,-1}; //used by evaluator helper
+	
+	//this is the value of a given number of same symbols in a row with a given amount of open space around it
+	//the first index is number of open spaces (0 to 4) and the second index is number in a row minus one (1 to 5)
+	//TODO: Refine these numbers. What I have here is a guess as to what's good.
+	private static final int ROW_SCORES[][] = {{0, 0,  0,   0,    1000000},
+										   	   {0, 0,  0,   120,  1000000},
+										   	   {0, 0,  60,  1000, 1000000},
+										   	   {0, 10, 150, 1000, 1000000},
+										   	   {0, 40, 250, 1000, 1000000}};
+	
+	/**
+	 * Initializes as a copy of the supplied board.
+	 */
+	public BoardFiniteBot(BoardFiniteBot b){
+		this.xDim = b.xDim;
+		this.yDim = b.yDim;
+		this.board = b.board;
+		this.turn = b.turn;
+	}
+	
+	/**
+	 * Evaluates the strength of the board position for the current player.
+	 * Normally an evaluation function for minimax search is supposed to evaluate only static position,
+	 * for example a material advantage in Chess. Unfortunately, there is no such thing as a material advantage
+	 * in Tic Tac Toe, so this function has to look at 2, 3, and 4 in a rows to provide an estimate.
+	 * @return an integer evaluation of the strength of the current position.
+	 * 		   Positive is better for the current player.
+	 */
+	public int evaluate(){
+		hasChecked = new boolean[xDim][yDim];
+		for(int x=0; x<xDim; x++){
+			for(int y=0; y<yDim; y++){
+				hasChecked[x][y] = false;
+			}
+		}
+		
+		int result = 0;
+		Symbol s;
+		//This function could be made simpler but slower by use of squareSet()
+		//Unfortunately, speed matters for a minimax evaluator function, so I'll be rewriting a lot here.
+		for(int x=0; x<xDim; x++){
+			for(int y=0; y<yDim; y++){
+				if(hasChecked[x][y])
+					continue;
+				s = board[x][y];
+				if(s == Symbol.EMPTY)
+					continue;
+				else if(s == turn)
+					result += evaluateSquare(x,y);
+				else
+					result -= evaluateSquare(x,y);
+			}
+		}
+		
+		return result;
+	}
+	
+	/*
+	 * Helper for evaluate() 
+	 */
+	private int evaluateSquare(int x, int y){
+		int result = 0;
+		hasChecked[x][y] = true;
+		Symbol s = board[x][y];
+		Symbol curr;
+		int sameInARow;
+		int openSpaces;
+		boolean onOpenSpace;
+		int dx,dy;
+		
+		//cycle through the 4 directions represented by the 'directions' array.
+		//the other 4 cardinal directions handled because this loop looks at 'lines' (both directions).
+		for(int i=0; i<DIRECTIONS.length; i+=2){ 
+			sameInARow = 1;
+			openSpaces = 0;
+			onOpenSpace = false;
+			dx = DIRECTIONS[i];
+			dy = DIRECTIONS[i+1];
+			for(int ex = x+dx, ey = y+dy; ex>=0 && ex<xDim && ey>=0 && ey<yDim; ex+=dx, ey+=dy){
+				curr = board[ex][ey];
+				if(onOpenSpace && (curr == Symbol.EMPTY || curr == s)){ //counts same symbol after a gap as open for evaluation
+					openSpaces++;
+					break;
+				}
+				
+				if(curr == Symbol.EMPTY){
+					openSpaces++;
+					onOpenSpace = true;
+				}
+				else if(curr == s){
+					sameInARow++;
+					hasChecked[ex][ey] = true;
+				}
+				else{ //ran into opposite color symbol
+					break;
+				}
+			}
+			onOpenSpace = false;
+			//here's the repeat in the opposite direction
+			for(int ex = x-dx, ey = y-dy; ex>=0 && ex<xDim && ey>=0 && ey<yDim; ex-=dx, ey-=dy){
+				curr = board[ex][ey];
+				if(onOpenSpace && (curr == Symbol.EMPTY || curr == s)){ //counts same symbol after a gap as open for evaluation
+					openSpaces++;
+					break;
+				}
+				
+				if(curr == Symbol.EMPTY){
+					openSpaces++;
+					onOpenSpace = true;
+				}
+				else if(curr == s){
+					sameInARow++;
+					hasChecked[ex][ey] = true;
+				}
+				else{ //ran into opposite color symbol
+					break;
+				}
+			}
+			result += ROW_SCORES[openSpaces][sameInARow-1];
+		}
+		
+		return result;
+	}
+	
+	public BoardFiniteBot afterMove(Square move){
+		BoardFiniteBot result = new BoardFiniteBot(this);
+		result.makeMove(move);
+		return result;
+	}
+	
+	/**
+	 * Provides a list of all "reasonable" squares to use as the current player's move,
+	 * which is to say all empty squares within a line of length two of an already existing symbol.
+	 * If the board is empty, instead gives a list containing only a square at/near the center of the board.
+	 * 
+	 * @return A list of Squares representing legal moves near the current area of play. 
+	 */
+	public List<Square> getAllReasonableMoves(){
+		List<Square> result = new ArrayList<Square>(50); //TODO: Check if this is reasonable starting size
+		
+		hasChecked = new boolean[xDim][yDim];
+		for(int x=0; x<xDim; x++){
+			for(int y=0; y<yDim; y++){
+				hasChecked[x][y] = false;
+			}
+		}
+		
+		for(int x=0; x<xDim; x++){
+			for(int y=0; y<yDim; y++){
+				if(board[x][y] != Symbol.EMPTY){
+					getReasonableMovesFromSquare(x,y,result);
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	/*
+	 * Helper method for getAllReasonableMoves()
+	 * adds to l as a side effect
+	 */
+	private void getReasonableMovesFromSquare(int x, int y, List<Square> l){
+		int ex,ey;
+		//check all directions
+		for(int dx=-1; dx<2; dx++){
+			for(int dy=-1; dy<2; dy++){
+				if(dx == 0 && dy == 0) //nowhere isn't a direction!
+					continue;
+				ex = x+dx;
+				ey = y+dy;
+				//can't move off the board or on an occupied square
+				if(ex<0 || ex>=xDim || ey<0 || ey>=yDim || board[ex][ey] != Symbol.EMPTY)
+					continue;
+				if(!hasChecked[ex][ey]){
+					l.add(new Square(ex,ey));
+					hasChecked[ex][ey] = true;
+				}
+				//and check one more in the same direction. Sorry about the repeated code )=
+				ex+=dx;
+				ey+=dy;
+				//can't move off the board or on an occupied square
+				if(ex<0 || ex>=xDim || ey<0 || ey>=yDim || board[ex][ey] != Symbol.EMPTY)
+					continue;
+				if(!hasChecked[ex][ey]){
+					l.add(new Square(ex,ey));
+					hasChecked[ex][ey] = true;
+				}
+			}
+		}
+	}
+	
+}
---
+++ FiveTicTacToe/src/ticTacToeBot/TicTacToeABTree.java
@@ -0,0 +1,116 @@
+package ticTacToeBot;
+
+import java.util.List;
+import backEnd.Square;
+
+/**
+ * TicTacToeABTree.java
+ * A Minimax tree with alpha-beta pruning and iterative deepening for searching the game tree.
+ * This class is mostly copy-pasta'd from the ABTree I wrote for my chessbot.
+ * Obviously I changed a number of methods to make it fit.
+ * 
+ * @author Kelton Finch
+ */
+class TicTacToeABTree {
+
+	private int fixedDepth;
+	private Square bestMove;
+	
+	//The switch for verbose mode, and some vars for keeping track of stats to be printed.
+	private boolean verbose;
+	private int posEvalCount;
+	
+	public TicTacToeABTree(){
+		verbose = false;
+	}
+	
+	public TicTacToeABTree(boolean verbose){
+		this.verbose = verbose;
+	}
+
+	/**
+	 * Searches to the specified depth starting from the specified board position, and returns the best move found.
+	 */
+	public Square getBestMoveFixed(BoardFiniteBot b, int depth) {
+		posEvalCount = 0;
+		bestMove = null;
+		
+		long beginTime, endTime;
+		beginTime = System.nanoTime();
+		//TODO: Search clearly slows WAY down when nearing OOM without doing much useful work. Investigate possible fixes.
+		try{
+			//search is progressively deepened, with the best move from previous iterations searched first.
+			for(int i=2; i<=depth; i++){
+				if(verbose)
+					System.out.println("Searching at depth " + i + "...");
+				fixedDepth = i;
+				treeSearchRecurse(b,i,-Integer.MAX_VALUE,Integer.MAX_VALUE);
+				if(verbose)
+					System.out.println("Current best move found: " + bestMove);
+			}
+		//if we OOM during a tree search, just return our best working solution.
+		} catch (OutOfMemoryError e){
+			if(verbose)
+				System.out.println("Ran out of memory! Returning best working solution.");
+		}
+		endTime = System.nanoTime();
+		
+		if(verbose){
+			System.out.println("Search took " + ((endTime - beginTime)/1000000000) + " seconds");
+			System.out.println("Positions evaluated: " + posEvalCount);
+		}
+		return bestMove;
+	}
+	
+	/*
+	 * Recursive helper for getBestMoveFixed. Performs an alpha-beta pruned minimax tree search.
+	 * Optimizes search time via a transposition table.
+	 * Modifies bestMove as a side effect.
+	 */
+	private int treeSearchRecurse(BoardFiniteBot b, int depth, int alpha, int beta){
+		posEvalCount++;
+		
+		//This comes before transposition table stuff because attempting to use 
+		//transposition tables at depth 0 results in pretty immediate OOM.
+		if(depth == 0)
+			return b.evaluate();
+		
+		List<Square> sl = b.getAllReasonableMoves();
+		
+		if(sl.isEmpty()) //i.e. board is full, so it's a stalemate
+			return 0;
+		
+		if(b.fiveInARow() != null) //i.e. the guy who just moved won, so this is worst position for current player
+			return -Integer.MAX_VALUE;
+			
+		int curr;
+		
+		//If this is top call, search current best move first.
+		//This produces more sensible behavior when the search must be stopped early due to running out of memory or time.
+		//In addition, searching the probable best position first usually results in much more AB pruning.
+		//This will cause minor amounts of extra work because no transposition table is implemented here.
+		if(fixedDepth == depth && bestMove != null){
+			curr = -treeSearchRecurse(b.afterMove(bestMove),depth-1,-beta,-alpha);
+			if(curr > alpha)
+				alpha = curr;
+			//no need to set bestMove here: it already is bestMove.
+		}
+		
+		//Recursively searches all possible moves from this position, looking for the best one.
+		for(Square s : sl){
+			curr = -treeSearchRecurse(b.afterMove(s),depth-1,-beta,-alpha);  
+			if(curr > alpha){
+				alpha = curr;
+				if(fixedDepth == depth)
+					bestMove = s;
+			}
+			if(alpha >= beta)
+				break;
+		}
+		
+		return alpha;
+	}
+
+	
+}
+
